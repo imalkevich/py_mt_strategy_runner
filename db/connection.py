d7bdd0d9d84acbae78832f60bdcc9a08d4f81f42
Ihar Malkevich <Ihar_Malkevich@epam.com>
2018-04-16 02:09:25 -0500

    more fixes for Travis-CI

--- db/connection.py
+++ db/connection.py
@@ -3,12 +3,13 @@
 import pyodbc
 from util.config import config
 
-_server = config.get('mssql', 'server')
-_database = config.get('mssql', 'database')
-_username = config.get('mssql', 'username')
-_password = config.get('mssql', 'password')
-_driver= '{ODBC Driver 13 for SQL Server}'
-_cnxn = pyodbc.connect('DRIVER='+_driver+';PORT=1433;SERVER='+_server+';PORT=1443;DATABASE='+_database+';UID='+_username+';PWD='+ _password)
+if config.has_section("mssql"):
+    _server = config.get('mssql', 'server')
+    _database = config.get('mssql', 'database')
+    _username = config.get('mssql', 'username')
+    _password = config.get('mssql', 'password')
+    _driver= '{ODBC Driver 13 for SQL Server}'
+    _cnxn = pyodbc.connect('DRIVER='+_driver+';PORT=1433;SERVER='+_server+';PORT=1443;DATABASE='+_database+';UID='+_username+';PWD='+ _password)
 
 def get_connection():
     """ get connection """
