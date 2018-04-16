e7c534ddedf0ba2db010d2d3198b8149c25d7dda
Ihar Malkevich <Ihar_Malkevich@epam.com>
2018-04-16 02:06:15 -0500

    more fixes for Travis-CI

--- util/terminal.py
+++ util/terminal.py
@@ -2,4 +2,7 @@
 
 from .config import config
 
-TERMINAL_POOL = [{'exe_path': t[0], 'data_path': t[1] } for t in [t.split('|') for t in config.get("terminal", "pool").split(",")]]
+TERMINAL_POOL = []
+
+if config.has_section("terminal"):
+    TERMINAL_POOL = [{'exe_path': t[0], 'data_path': t[1] } for t in [t.split('|') for t in config.get("terminal", "pool").split(",")]]
